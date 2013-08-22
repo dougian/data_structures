baf639abe9722653f6628cc66743eef32a54aa6b
John Douratsos <dougian2@gmail.com>
2013-08-22 03:12:47 -0700

    still needs some tinkering, sizes don't work

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 import time
 import os
@@ -14,70 +15,84 @@ test_items = []
 start = [0,0,0,0,0,0,0,0]
 end = [0,0,0,0,0,0,0]
 avg = [0,0,0,0,0,0,0]
-endresults = []
+endresults = {}
+debug = False
+
 with open("sample.txt") as f:
     test_items = pickle.load(f)
-for (iid, ttitle, ssurname) in test_items[:2]:
+
+for size in range(500,1000,500):
+    print("Size is ",size)
     b = bookservice.Books()
-    b.load_books("books.csv",int(lines))
-    print("PSAXNW TO {}",iid)
+    b.load_books("books.csv",size)
     t = trie.Trie()
     t2 = trie.Trie()
     avl = AVL.AVLTree()
     avl = AVL.AVLTree()
-    print("creating trie + AVL....")
+    if debug: print("creating trie + AVL....")
     t.build_tree(b.arr)
     avl._buildMe(b.arr)
     t2.build_authors(b.arr)
-    print("I'm ready, LET'S TEST!")
+    if debug: print("I'm ready, LET'S TEST!")
 
-    start[0] = time.time()
-    res1 = b.disp_id(iid)
-    end[0] = time.time()
+    for (iid, ttitle, ssurname) in test_items:
+        if debug: print("PSAXNW TO {}".format(iid))
+        start[0] = time.time()
+        res1 = b.disp_id(iid)
+        end[0] = time.time()
 
-    start[1] = time.time()
-    res2 = b.binary_search(iid)
-    end[1] = time.time()
+        start[1] = time.time()
+        res2 = b.binary_search(iid)
+        end[1] = time.time()
 
-    start[2] = time.time()
-    res3 = t.lookup(ttitle)
-    end[2] = time.time()
+        start[2] = time.time()
+        res3 = t.lookup(ttitle)
+        end[2] = time.time()
 
-    start[3] = time.time()
-    res4 = b.disp_title(ttitle)
-    end[3] = time.time()
+        start[3] = time.time()
+        res4 = b.disp_title(ttitle)
+        end[3] = time.time()
 
-    start[4] = time.time()
-    res5 = avl.find(iid)
-    end[4] = time.time()
+        start[4] = time.time()
+        res5 = avl.find(iid)
+        end[4] = time.time()
 
-    start[5] = time.time()
-    temp = [str(i) for i in b.disp_surname(ssurname)]
-    res5 = " ".join(temp)
-    end[5] = time.time()
+        start[5] = time.time()
+        ll1 = b.disp_surname(ssurname)
+        if ll1 is not None:
+            temp = [str(i) for i in ll1 if ll1]
+            res5 = " ".join(temp)
+        else:
+            res5 = None
+        end[5] = time.time()
 
-    start[6] = time.time()
-    temp2 = [str(i) for i in t2.lookup(ssurname)]
-    res6 = " ".join(temp2)
-    end[6] = time.time()
-    avg = [a + e - s for a, e, s in zip(avg,end,start)]
+        start[6] = time.time()
+        ll2 = t2.lookup(ssurname)
+        if ll2 is not None:
+            temp2 = [str(i) for i in ll2]
+            res6 = " ".join(temp2)
+        else:
+            res6 = None
+        end[6] = time.time()
+        avg = [a + e - s for a, e, s in zip(avg,end,start)]
 
-    if checkResults :
-        print("All results are EQUAL!!!!")
-    else:
-        print("Error!")
-        exit()
-        endresults.append(size,list(avg))  #create a copy of avg and store it
+        if not checkResults :
+            print("Error!")
+            exit()
+        else:
+            endresults[size] = copy.deepcopy(avg)  #create a copy of avg and store it
 
+print("endresults:", endresults)
+for item in endresults:
 
-print("===============Times===============")
-print("id Linear search:", avg[0])
-print("id Binary search : ",avg[1] )
-print("id avl search : ", avg[4])
-print("----------------------------------")
-print("string trie search : ",avg[2])
-print("string linear search: " ,avg[3])
-print("----------------------------------")
-print("surname linear: ", avg[5])
-print("surname trie: ", avg[6])
+    print("===============Times===============")
+    print("id Linear search:", endresults[item][0])
+    print("id Binary search : ",endresults[item][1] )
+    print("id avl search : ", endresults[item][4])
+    print("----------------------------------")
+    print("string trie search : ",endresults[item][2])
+    print("string linear search: " ,endresults[item][3])
+    print("----------------------------------")
+    print("surname linear: ", endresults[i][5])
+    print("surname trie: ", endresults[i][6])
 
